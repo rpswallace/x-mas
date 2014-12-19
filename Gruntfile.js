79e018192a661fbc09bea5ff37957329c4348752
Randy Pasos <randyp@thehangar.cr>
2014-12-19 12:35:25 -0600

    add header & fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -347,7 +347,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'fonts/{,*/}*.*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
--- dist/scripts/vendor/preload/filesmanifest.js
+++ dist/scripts/vendor/preload/filesmanifest.js
@@ -2,33 +2,43 @@
 	"filesManifest": {
 		"files": [
 			{
-				"id": "ImagesDdb0491d.basketballCourt",
-			    "src": "/images/ddb0491d.basketball_court.jpg",
+				"id": "ImagesAf916d55.xmasBgDummy",
+			    "src": "/images/af916d55.xmas-bg-dummy.png",
 			    "type": "IMAGE"
 	        },
 			{
-				"id": "ImagesGifs5384dc22.image4",
-			    "src": "/images/gifs/5384dc22.image4.gif",
+				"id": "ImagesGifs2a9027cb.xmasDummy07",
+			    "src": "/images/gifs/2a9027cb.xmas-dummy_07.gif",
 			    "type": "IMAGE"
 	        },
 			{
-				"id": "ImagesGifs98651e92.image3",
-			    "src": "/images/gifs/98651e92.image3.gif",
+				"id": "ImagesGifs39ab955f.xmasDummy06",
+			    "src": "/images/gifs/39ab955f.xmas-dummy_06.gif",
 			    "type": "IMAGE"
 	        },
 			{
-				"id": "ImagesGifsDae4f201.image5",
-			    "src": "/images/gifs/dae4f201.image5.gif",
+				"id": "ImagesGifs5ae4ae5d.xmasDummy05",
+			    "src": "/images/gifs/5ae4ae5d.xmas-dummy_05.gif",
 			    "type": "IMAGE"
 	        },
 			{
-				"id": "ImagesGifsDe9a513f.image2",
-			    "src": "/images/gifs/de9a513f.image2.gif",
+				"id": "ImagesGifs93ca57a0.xmasDummy03",
+			    "src": "/images/gifs/93ca57a0.xmas-dummy_03.gif",
 			    "type": "IMAGE"
 	        },
 			{
-				"id": "ImagesGifsF9d3fcea.image1",
-			    "src": "/images/gifs/f9d3fcea.image1.gif",
+				"id": "ImagesGifsAc6ba887.xmasDummy04",
+			    "src": "/images/gifs/ac6ba887.xmas-dummy_04.gif",
+			    "type": "IMAGE"
+	        },
+			{
+				"id": "ImagesGifsB42bb88b.xmasDummy01",
+			    "src": "/images/gifs/b42bb88b.xmas-dummy_01.gif",
+			    "type": "IMAGE"
+	        },
+			{
+				"id": "ImagesGifsCe954ae9.xmasDummy02",
+			    "src": "/images/gifs/ce954ae9.xmas-dummy_02.gif",
 			    "type": "IMAGE"
 	        }
         ]
